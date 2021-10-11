5b556cf69be2c7969e8fba12274281911ca577df
Luke Van Seters <lukevanseters@gmail.com>
2021-10-11 10:49:53 -0700

    Make tests more accurate - currently fails

--- tests/test_arbitrage_integration.py
+++ tests/test_arbitrage_integration.py
@@ -1,5 +1,8 @@
+from typing import List
+
 from mev_inspect.arbitrages import get_arbitrages
 from mev_inspect.classifiers.trace import TraceClassifier
+from mev_inspect.schemas.classified_traces import Classification, ClassifiedTrace
 from mev_inspect.swaps import get_swaps
 from mev_inspect.transfers import get_transfers
 
@@ -12,10 +15,14 @@ def test_arbitrage_real_block():
     trace_clasifier = TraceClassifier()
     classified_traces = trace_clasifier.classify(block.traces)
 
+    total_swap_traces = total_traces_of_classification(
+        classified_traces, Classification.swap
+    )
+
     transfers = get_transfers(classified_traces)
 
     swaps = get_swaps(classified_traces, transfers)
-    assert len(swaps) == 49
+    assert len(swaps) == total_swap_traces
 
     arbitrages = get_arbitrages(list(swaps))
     assert len(arbitrages) == 1
@@ -27,3 +34,9 @@ def test_arbitrage_real_block():
         arbitrage.profit_token_address == "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"
     )
     assert arbitrage.profit_amount == 53560707941943273628
+
+
+def total_traces_of_classification(
+    traces: List[ClassifiedTrace], classification: Classification
+) -> int:
+    return sum(1 for trace in traces if trace.classification == classification)
